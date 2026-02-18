0d1f10b457fbdd62fc0e5d20cf9b82d540779087
Marcin Sachs <marcinsachs@gmail.com>
2026-02-18 19:11:52 +0100

    Rebuild merge_resolve_cycle down_revision list with actual revision IDs

--- migrations/versions/merge_resolve_cycle.py
+++ migrations/versions/merge_resolve_cycle.py
@@ -1,7 +1,7 @@
 """
 Merge migration to resolve cycles in history
 Revision ID: merge_resolve_cycle
-Revises: 1495638fbb48,27ad0c4ba539,42404df2530c,42bd4f2a029b,58947c6ef9f7,6e53fc0c06b1,80c2cb49fa42,873d72b6bc6f,8904ea11c501,add_admin_super_admin_messaging,add_book_description,add_contact_message_id_to_notification,add_contact_message_table,add_invitation_codes,add_library_loan_overdue_days,add_premium_features_to_tenant,add_subdomain_to_tenant,add_user_names,b1c7781652f9,c2477abce1cb,ee2182cf18f2,f1215d41dff2,make_isbn_nullable,make_tenant_id_nullable_for_user,multi_tenant_2026,simple_password_hash_fix,update_contact_message_for_conversations
+Revises: 1495638fbb48,27ad0c4ba539,42404df2530c,42bd4f2a029b,58947c6ef9f7,6e53fc0c06b1,80c2cb49fa42,873d72b6bc6f,8904ea11c501,add_admin_super_admin_messaging,123456789abc,add_contact_message_id,add_contact_message_table,add_invitation_codes,add_library_loan_overdue_days,add_premium_features,add_subdomain_to_tenant,add_user_names,b1c7781652f9,c2477abce1cb,ee2182cf18f2,f1215d41dff2,make_isbn_nullable,make_tenant_id_nullable_for_user,multi_tenant_2026,simple_password_hash_fix,update_contact_msg_conv
 Create Date: 2026-02-18
 """
 from alembic import op
@@ -11,10 +11,10 @@ import sqlalchemy as sa
 revision = 'merge_resolve_cycle'
 down_revision = (
     '1495638fbb48','27ad0c4ba539','42404df2530c','42bd4f2a029b','58947c6ef9f7','6e53fc0c06b1','80c2cb49fa42',
-    '873d72b6bc6f','8904ea11c501','add_admin_super_admin_messaging','add_book_description','add_contact_message_id_to_notification',
-    'add_contact_message_table','add_invitation_codes','add_library_loan_overdue_days','add_premium_features_to_tenant',
+    '873d72b6bc6f','8904ea11c501','add_admin_super_admin_messaging','123456789abc','add_contact_message_id',
+    'add_contact_message_table','add_invitation_codes','add_library_loan_overdue_days','add_premium_features',
     'add_subdomain_to_tenant','add_user_names','b1c7781652f9','c2477abce1cb','ee2182cf18f2','f1215d41dff2','make_isbn_nullable',
-    'make_tenant_id_nullable_for_user','multi_tenant_2026','simple_password_hash_fix','update_contact_message_for_conversations'
+    'make_tenant_id_nullable_for_user','multi_tenant_2026','simple_password_hash_fix','update_contact_msg_conv'
 )
 branch_labels = None
 depends_on = None
